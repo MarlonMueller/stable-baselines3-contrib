8820a8a47a5af2a76b46be42876a8dff0df0bfb7
MarlonMueller <marlon.mueller@tum.de>
2021-08-20 15:47:07 +0200

    a2c_mask

---
+++ sb3_contrib/a2c_mask/__init__.py
@@ -0,0 +1,5 @@
+from sb3_contrib.ppo_mask.policies import CnnPolicy, MlpPolicy, MultiInputPolicy
+from sb3_contrib.ppo_mask.ppo_mask import MaskablePPO
+
+from sb3_contrib.a2c_mask.policies import CnnPolicy, MlpPolicy, MultiInputPolicy
+from sb3_contrib.a2c_mask.a2c_mask import MaskableA2C
