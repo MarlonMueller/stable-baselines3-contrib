9b912116c9d30835e3a1dbc6bab02a4e7fe52cfb
MarlonMueller <marlon.mueller@tum.de>
2021-09-30 03:37:51 +0200

    MaskPunish

--- thesis/train.sh
+++ thesis/train.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for i in {0..29} #29
+for i in {15..17} #29
 do
         echo "Creating session $i"
         tmux new-session -d -s $i "source activate safety_wrappers; taskset --cpu-list $i python3.8 -m thesis.main --flag $i";
