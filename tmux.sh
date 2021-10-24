e17c631060451bfac912ab721efb898ad873140a
MarlonMueller <marlon.mueller@tum.de>
2021-10-24 23:04:47 +0200

    CleanUp

--- tmux.sh
+++ tmux.sh
@@ -3,6 +3,6 @@
 for i in {0..127}
 do
         # Distribute main calls to isolated cores
-        tmux new-session -d -s $i "source activate safetyWrappers; taskset --cpu-list $i python3.8 -m main --flag $i";
+        tmux new-session -d -s $i "source activate safetyWrappers; taskset --cpu-list $i python3.8 main.py --flag $i";
 done
 
