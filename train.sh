03ba8aa8295c10ca6ec41e70082c49ebda23c971
MarlonMueller <marlon.mueller@tum.de>
2021-10-24 21:14:51 +0200

    CleanUp

--- train.sh
+++ train.sh
@@ -3,6 +3,6 @@
 for i in {0..127}
 do
         # Distribute main calls to isolated cores
-        tmux new-session -d -s $i "source activate safety_wrappers; taskset --cpu-list $i python3.8 -m thesis.main --flag $i";
+        tmux new-session -d -s $i "source activate safety_wrappers; taskset --cpu-list $i python3.8 -m main --flag $i";
 done
 
